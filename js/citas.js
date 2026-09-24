30cfa7741000c09fd41d289deaa93fbb29b06c7b
mikagiuri <mikagiuri@gmail.com>
2026-09-24 19:18:47 +0200

    Versión alumnado (2026-09-24)

--- js/citas.js
+++ js/citas.js
@@ -423,7 +423,9 @@ const CITAS = [
   "c": "La Ilustración es totalitaria.",
   "a": "Adorno & Horkheimer",
   "o": "Razón instrumental (Dialéctica de la Ilustración)",
-  "e": "contemporanea"
+  "e": "contemporanea",
+  "id": "adorno",
+  "img": "media/retratos/ilustres/adorno.jpg"
  },
  {
   "c": "Lo eternamente incomprensible del mundo es su comprensibilidad.",
